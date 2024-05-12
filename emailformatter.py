14c3798e5dfb7f899169d97f17084dfa016d829e
ejones001 <ejonesnavy@gmail.com>
2024-05-12 00:56:53 -0500

    Email formatter

---
+++ emailformatter.py
@@ -0,0 +1,8 @@
+def email_formatter(email):
+    
+    formatted_email = email.lower()
+    print("Formatted email:", formatted_email)
+
+
+email = input("Enter your email address: ")
+email_formatter(email)
